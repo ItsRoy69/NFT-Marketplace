79ef73505186e9a8289c65b2395fbd2fd6270d01
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-11-10 10:05:04 +0530

    Installation of components and constants

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
@@ -18,19 +17,17 @@ const theme = {
 const Stack = createStackNavigator();
 
 const App = () => {
-
   const [loaded] = useFonts({
     InterBold: require("./assets/fonts/Inter-Bold.ttf"),
     InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
     InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
     InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
-    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf")
   });
 
   if (!loaded) return null;
 
   return (
-
     <NavigationContainer theme={theme}>
       <Stack.Navigator
         screenOptions={{
@@ -42,8 +39,7 @@ const App = () => {
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
-  
   );
-}
+};
 
 export default App;
---
+++ components/index.js
@@ -0,0 +1,9 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+
+export {
+  HomeHeader,
+  NFTCard,  
+  FocusedStatusBar,
+};
