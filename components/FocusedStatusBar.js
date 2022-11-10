0092ce38d10ff92c04c491d88c8df427771ea738
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-11-10 10:44:38 +0530

    Setting up homepagge, like button, and a navbar

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,12 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StatusBar } from "react-native";
+import { useIsFocused } from "@react-navigation/core";
 
-const FocusedStatusBar = () => {
-  return (
-    <View>
-      <Text>FocusedStatusBar</Text>
-    </View>
-  )
-}
-
-export default FocusedStatusBar
+const FocusedStatusBar = (props) => {
+    const isFocused = useIsFocused();
+  
+    return isFocused ? <StatusBar animated={true} {...props} /> : null;
+  };
+  
+  export default FocusedStatusBar;
