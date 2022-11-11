355da5e8fea6a2591d2eed2977a9b8c61781c0b5
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-11-11 18:46:58 +0530

    Application folder and NFT Details

--- Application/screens/Home.js
+++ Application/screens/Home.js
@@ -1,22 +1,39 @@
 import React, { useState } from "react";
-import { View, SafeAreaView, FlatList, Text } from "react-native";
+import { View, SafeAreaView, FlatList } from "react-native";
 
-import { COLORS, NFTData } from "../constants";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (value.length === 0) {
+      setNftData(NFTData);
+    }
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length === 0) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-
       <FocusedStatusBar backgroundColor={COLORS.primary} />
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
-            renderItem={({ item }) => <NFTCard data={item} /> }
+            data={nftData}
+            renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
         </View>
 
@@ -35,9 +52,8 @@ const Home = () => {
           <View style={{ flex: 1, backgroundColor: COLORS.white }} />
         </View>
       </View>
-      
-    </SafeAreaView> 
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
